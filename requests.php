589c7869a018105b4eab306e4fbd45bb34291a5b
A8_Mac Luna <macluna94@gmail.com>
2017-12-04 03:45:00 -0600

    Request
    Reduccion de lineas

--- requests.php
+++ requests.php
@@ -4,7 +4,6 @@
 	$filtro = new InputFilter();
 	$_POST = $filtro->process($_POST);
 ?>
- 
 <!DOCTYPE html>
 <html lang="es" dir="ltr">
 	<head>
@@ -120,145 +119,135 @@
 					<div class="panel panel-default" id="itinerario-panel">
 						<div class="panel-heading">
 							<h4><label><strong> Itinerario</strong></label></h4>
-</div>
-<div class="panel-body">
-<div class="form-group row">
-<div class="col-xs-4">
-	<label class="control-label">Lugar de destino o arribo</label>
-		<input  type="text" for="solicitud" name="evento"  class="form-control" required placeholder="Lugar del evento">
-</div>
-
-<div class="col-xs-4">
-	<div class="form-group">
-		<label class="control-label">Estado</label>
-			<select required="true"  name="estados" id="estados" for="solicitud" class="form-control" onchange="showState(this.value);" >
-				<option></option>
-				<?php echo $option_state; ?>
-			</select>
-	</div>
-
-		<script>
-			function showState(str) {
-			if (str == "") {
-				document.getElementById("selectEstado").innerHTML = "";
-				return;
-			}
-			else { 
-				if (window.XMLHttpRequest) {
-					xmlhttp = new XMLHttpRequest();
-				}
-				else {
-					xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-				}
-				xmlhttp.onreadystatechange = function() {
-				if (this.readyState == 4 && this.status == 200) {
-						document.getElementById("selectEstado").innerHTML = this.responseText;
-					}
-				};
-				xmlhttp.open("GET","php/municipios.php?estado="+str,true);
-				xmlhttp.send();
-			}
-			//alert("Estado: "+str);
-			$("#mncp").remove();
-			}
-		</script>
-</div>
-
-<div class="col-xs-4">
-	<div class="form-group">
-		<div id="selectEstado"></div>
-
-		<div id="mncp">										
-			<label for="solicitud">Municipio:</label>
-				<select name="conductor" class="form-control" >
-					<option>
-					</option>
-				</select>
-		</div>
-	</div>
-</div>
-
-</div>
-<div class="form-group row">
-<div class="col-xs-6">
-	<div class="form-group">
-<script>
-	$(document).ready(function(){
-				$("#pdf").hide();
-    			$("#xml").hide();
-	});
-</script>
-
-<script>
-function showPrograms(hub){
-	    	if (hub != "1" ) {
-	    		 
-	            document.getElementById("selectPrograma").innerHTML = "";
-	            return;
-	        } 
-	        else { 
-	            if (window.XMLHttpRequest) {
-	                newxhttp = new XMLHttpRequest();
-	            } else {
-	                newxhttp = new ActiveXObject("Microsoft.XMLHTTP");
-	            }
-	            newxhttp.onreadystatechange = function() {
-	                if (this.readyState == 4 && this.status == 200) {
-	                    document.getElementById("selectPrograma").innerHTML = this.responseText;
-	                }
-	            };
-	            newxhttp.open("GET","php/programas.php?programa="+hub,true);
-	            newxhttp.send();
-	        }
-    	}
-    	function sub_docs(hub){
-    		console.log(hub);
-    		if (hub == 1) {
-    			$("#pdf").show();
-    			$("#xml").show();
-$("input[id=file-pdf]").attr("required", true);
-$("input[id=file-xml]").attr("required", true);
-    		}
-    		else if (hub == 3) {
-    			$("#pdf").show();
-    			$("input[id=file-pdf]").attr("required", true);
-    			$("input[id=file-xml]").attr("required", false);
-    		}
-    		else if(hub == 6){
-				$("#xml").show();
-				$("input[id=file-xml]").attr("required", true);
-				$("input[id=file-pdf]").attr("required", false);
-    		}
-    		else{
-    			$("#pdf").hide();
-    			$("#xml").hide();
-    			$("input[id=file-pdf]").attr("required", false);
-				$("input[id=file-xml]").attr("required", false);
-    		}
-    	}
-</script>
-
-		<label for="sel2">Ejes:</label>
-			<select  name="labor" class="form-control" onchange="showPrograms(this.value);sub_docs(this.value);" required>
-				<option></option>
-				<?php echo $options_hubs; ?>
-			</select>
-	</div>
-</div>
-
-			<div class="col-xs-6">
-				<div id="selectPrograma" class=""></div>
-			</div>
-		</div>
+						</div>
+						<div class="panel-body">
 							<div class="form-group row">
-								
+								<div class="col-xs-4">
+									<label class="control-label">Lugar de destino o arribo</label>
+										<input  type="text" for="solicitud" name="evento"  class="form-control" required placeholder="Lugar del evento">
+								</div>
+
+								<div class="col-xs-4">
+									<div class="form-group">
+										<label class="control-label">Estado</label>
+											<select required="true"  name="estados" id="estados" for="solicitud" class="form-control" onchange="showState(this.value);" >
+												<option></option>
+												<?php echo $option_state; ?>
+											</select>
+									</div>
+									<script>
+										function showState(str) {
+											if (str == "") {
+												document.getElementById("selectEstado").innerHTML = "";
+												return;
+											}
+											else { 
+												if (window.XMLHttpRequest) {
+													xmlhttp = new XMLHttpRequest();
+												}
+												else {
+													xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+												}
+												xmlhttp.onreadystatechange = function() {
+												if (this.readyState == 4 && this.status == 200) {
+														document.getElementById("selectEstado").innerHTML = this.responseText;
+													}
+												};
+												xmlhttp.open("GET","php/municipios.php?estado="+str,true);
+												xmlhttp.send();
+											}
+											//alert("Estado: "+str);
+											$("#mncp").remove();
+										}
+									</script>
+								</div>
+
+								<div class="col-xs-4">
+									<div class="form-group">
+										<div id="selectEstado">
+										</div>
+										<div id="mncp">										
+											<label for="solicitud">Municipio:</label>
+												<select name="conductor" class="form-control" >
+													<option>
+													</option>
+												</select>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="form-group row">
+								<div class="col-xs-6">
+									<div class="form-group">
+										<script>
+											$(document).ready(function(){
+													$("#pdf").hide();
+										    		$("#xml").hide();
+											});
+											function showPrograms(hub){
+										    	if (hub != "1" ) {
+		            							document.getElementById("selectPrograma").innerHTML = "";
+		            							return;
+										       } 
+										      else { 
+										       	if (window.XMLHttpRequest) {
+															newxhttp = new XMLHttpRequest();
+														}
+													else {
+														newxhttp = new ActiveXObject("Microsoft.XMLHTTP");
+													}
+													newxhttp.onreadystatechange = function() {
+														if (this.readyState == 4 && this.status == 200) {
+															document.getElementById("selectPrograma").innerHTML = this.responseText;
+														}
+													};
+														newxhttp.open("GET","php/programas.php?programa="+hub,true);
+														newxhttp.send();
+												}
+											}
+											function sub_docs(hub){
+												if (hub == 1) {
+													$("#pdf").show();
+													$("#xml").show();
+													$("input[id=file-pdf]").attr("required", true);
+													$("input[id=file-xml]").attr("required", true);
+												}
+												else if (hub == 3) {
+													$("#pdf").show();
+													$("input[id=file-pdf]").attr("required", true);
+													$("input[id=file-xml]").attr("required", false);
+												}
+												else if(hub == 6){
+													$("#xml").show();
+													$("input[id=file-xml]").attr("required", true);
+													$("input[id=file-pdf]").attr("required", false);
+												}
+												else{
+									    			$("#pdf").hide();
+									    			$("#xml").hide();
+									    			$("input[id=file-pdf]").attr("required", false);
+													$("input[id=file-xml]").attr("required", false);
+	    										}
+	    									}
+										</script>
+									<label for="sel2">Ejes:</label>
+										<select  name="labor" class="form-control" onchange="showPrograms(this.value);sub_docs(this.value);" required>
+											<option></option>
+											<?php echo $options_hubs; ?>
+										</select>
+									</div>
+								</div>
+								<div class="col-xs-6">
+									<div id="selectPrograma" class=""></div>
+								</div>
+							</div>
+							<div class="form-group row">
 								<div class="col-xs-6">
 									<div class="form-group">
 										<label class="control-label">Lugar de Salida</label>
 										<input type="text"  for="solicitud"  name="salida" pattern="[A-Z-a-z-' ']+"  class="form-control" required>
 									</div>
 								</div>
-
 								<div class="col-xs-3">
 									<div class="form-group">
 										<label class="control-label">Fecha(Salida)</label>
@@ -271,7 +260,6 @@ $("input[id=file-xml]").attr("required", true);
 										<input type="time" name="h_salida" for="solicitud" value="00:00:00" class="form-control" required>
 									</div>
 								</div>
-
 								<div class="col-xs-6">
 								</div>
 								<div class="col-xs-3">
@@ -307,69 +295,65 @@ $("input[id=file-xml]").attr("required", true);
 							</div>
 						</div>
 					</div>
+
 					<div class="panel panel-default" id="transporte-panel">
 						<div class="panel-heading">						
-								<div class="form-group">
-								
-										<h4><strong>Transporte</strong></h4>
-										<br>
-							
-									</div>
-								</div>
-	<input id="transporte" type="hidden" name="transporte" value="" required>
-									
-							<div class="panel-body">
+							<div class="form-group">
+								<h4><strong>Transporte</strong></h4><br>
+							</div>
+						</div>
+							<input id="transporte" type="hidden" name="transporte" value="" required>		
+						<div class="panel-body">
 							<button  type="button" id="start" data-toggle="modal" class="btn btn-success"  data-target="#myModal"  value="MacLuna" >
 								<span class="glyphicon glyphicon-road"></span>   Seleccionar
 							</button>
 							<br>
 							<br>
-								<table class="table table-bordered">
-									<thead>
-										<tr class="info">
-											<th></th>
-											<th>Marca</th>
-											<th>Tipo</th>
-											<th>Capacidad</th>
-										</tr>
-									</thead>
-									<tbody>
-										<tr>
-											<td style="width: 68px;"> <img src="imgs/auto.png" width="32px" height="32px" style="margin-left: 8px;"> </td>
-											<td id="bb" style="text-align: center;" ></td>
-											<td id="tb" style="text-align: center;" ></td>
-											<td id="pb" style="text-align: center;" ></td>
-										</tr>
-									</tbody>
-								</table>
-								<div class="form-group row">
-									<div class="col-xs-6">
-										<div id="selectConductor" name="conductor">
-											<label for="sel1">Conductor: </label>
-												<select name="conductor" class="form-control"  required>
-													
+							<table class="table table-bordered">
+								<thead>
+									<tr class="info">
+										<th></th>
+										<th>Marca</th>
+										<th>Tipo</th>
+										<th>Capacidad</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td style="width: 68px;"> <img src="imgs/auto.png" width="32px" height="32px" style="margin-left: 8px;"> </td>
+										<td id="bb" style="text-align: center;" ></td>
+										<td id="tb" style="text-align: center;" ></td>
+										<td id="pb" style="text-align: center;" ></td>
+									</tr>
+								</tbody>
+							</table>
+							<div class="form-group row">
+								<div class="col-xs-6">
+									<div id="selectConductor" name="conductor">
+										<label for="sel1">Conductor: </label>
+											<select name="conductor" class="form-control"  required>
 												<option></option>
-												</select>
-										</div>
+											</select>
 									</div>
-									<div class="col-xs-6">
+								</div>
+								<div class="col-xs-6">
 										<div class="form-group">
 											<label for="sel1">Responsable:</label>
 												<select name = "responsable" class="form-control" required>
-<option></option>
+													<option></option>
 													<?php echo $options_responsable; ?>
 												</select>
 										</div>
-									</div>
 								</div>
 							</div>
+						</div>
 					</div>
+
 					<div class="panel panel-default" id="asistentes-panel">
 						<div class="panel-heading">
-								<h4> <strong>Relación de personas a transportar.</strong></h4>
+							<h4> <strong>Relación de personas a transportar.</strong></h4>
 						</div>
 						<div class="panel-body">
-						
 							<div class="form-group row">
 								<div class="col-xs-4"></div>
 								<div class="col-xs-4">
@@ -422,134 +406,98 @@ $("input[id=file-xml]").attr("required", true);
 							</div>
 						</div>
 					</div>
+
 					<div class="panel panel-default" id="gastos-panel">
 						<div class="panel-heading">	
-						<h4><label class="control-label">Gastos</label></h4>
+							<h4><label class="control-label">Gastos</label></h4>
 						</div>
 						<div class="panel-body">
-						
+							<div class="form-group row">
+								<div class="col-xs-4">
+									<label>Hospedaje: </label> <input type="text" name="hospedaje" value="" class="form-control" placeholder="$00.00" required="true">
+									<label>Alimentos: </label> <input type="text" name="alimentos" value="" class="form-control" placeholder="$00.00" required="true">
+								</div>
 
-<div class="form-group row">
-	<div class="col-xs-4">
-		
-	<label>Hospedaje: </label> <input type="text" name="hospedaje" value="" class="form-control" placeholder="$00.00" required="true">
-	<label>Alimentos: </label> <input type="text" name="alimentos" value="" class="form-control" placeholder="$00.00" required="true">
-	</div>
-	<div class="col-xs-4">
-		
-	<label>Peaje: </label> <input type="text" value="" name="peaje" class="form-control" placeholder="$00.00" required="true">
-	<label>Combustible: </label> <input type="text" name="combustible" value="" class="form-control" placeholder="$00.00" required="true">
-	</div>
-	<div class="col-xs-4">
-		
-	<label>Estacionamiento: </label> <input type="text" name="estacionamiento" value="" class="form-control" placeholder="$00.00" required="true">
-	<label>Estacionometro: </label> <input type="text" name="estacionometro" value="" class="form-control" placeholder="$00.00" required="true">
-	</div>
-</div>
+								<div class="col-xs-4">
+									<label>Peaje: </label> <input type="text" value="" name="peaje" class="form-control" placeholder="$00.00" required="true">
+									<label>Combustible: </label> <input type="text" name="combustible" value="" class="form-control" placeholder="$00.00" required="true">
+								</div>
+								<div class="col-xs-4">
+									<label>Estacionamiento: </label> <input type="text" name="estacionamiento" value="" class="form-control" placeholder="$00.00" required="true">
+									<label>Estacionometro: </label> <input type="text" name="estacionometro" value="" class="form-control" placeholder="$00.00" required="true">
+								</div>
+							</div>
 						</div>
 					</div>
-	<br>
-<div id="docs">
-	<div class="panel panel-default" id="documentos">
-		<div class="panel-heading">
-			<h4><label class="control-label">Documentos</label></h4>
-		</div>
-		<div class="panel-body">
-			<div class="form-group row">
-				<div id="pdf">
-					<div class="col-xs-6">
-						<label class="label-control">Subir PDF</label>
-						<input id="file-pdf" name="file-pdf" multiple type="file" required="false">
+					<br>
+					<div id="docs">
+						<div class="panel panel-default" id="documentos">
+							<div class="panel-heading">
+								<h4><label class="control-label">Documentos</label></h4>
+							</div>
+							<div class="panel-body">
+								<div class="form-group row">
+									<div id="pdf">
+										<div class="col-xs-6">
+											<label class="label-control">Subir PDF</label>
+											<input id="file-pdf" name="file-pdf" multiple type="file" required="false">
+										</div>
+									</div>
+									<div id="xml">
+										<div class="col-xs-6">
+											<label class="label-control">Subir XML</label>
+											<input id="file-xml" name="file-xml" multiple type="file" required="false">
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
 					</div>
-				</div>
-				<div id="xml">
-					<div class="col-xs-6">
-						<label class="label-control">Subir XML</label>
-						<input id="file-xml" name="file-xml" multiple type="file" required="false">
-					</div>
+					<input type="hidden" id="nombres" name="nombres"  >
+					<input type="hidden" id="codigos" name="codigos"  >
 				</div>
 			</div>
-		</div>
-	</div>
-</div>
-</div>
-
-
-<script>
-	
-
-
-</script>
-
-<script>
-	var state = false;
-	$("button[id=start]").click(function() {
-		state = true;
-		$("button[id=enviar]").attr("disabled", false);
-	})
-</script>
-
-<script>
-	console.log("Cargada funcion: \t mio()");
-
-	function mio(){
-	var valores="";
-	var codigos="";
-
-
-	$("td").parents("tr").find("#name").each(function(){
-	valores+=$(this).html()+"_";
-	});
-
-	$("td").parents("tr").find("#code").each(function(){
-	codigos+=$(this).html()+"_";
-	});
-
-	asistentes.push(valores);
-	a_codigos.push(codigos);
-	$("#codigos").val(codigos);
-	$("#nombres").val(valores);
-
-	console.log("Funcion mio() \t Ejecutada");
-	console.log("Nombre:"+asistentes+" Codigo: "+ a_codigos);
-
-
-	}
-
-	var asistentes = new Array();
-	var a_codigos = new Array();
-</script>
-
-<input type="hidden" id="nombres" name="nombres"  >
-<input type="hidden" id="codigos" name="codigos"  >
-
-
-
-
-					<div class="panel-footer">
-						<div class="form-group row">
-							<div class="col-xs-2">
-								<?php 
-								echo '<a href="principal.php?id='.$id.'" class="btn btn-warning" role="button">Cancelar</a>';
-								 ?>
-							</div>
-							<div class="col-xs-2"></div>
-							<div class="col-xs-2"></div>
-							<div class="col-xs-2"></div><div class="col-xs-2"></div>
-							<div class="col-xs-2">
-								<button type="submit" id="enviar" class="btn btn-success" disabled="true" onclick="mio()" >Enviar Solicitud</button>
-							</div>
-							
-						</div>
+			<div class="panel-footer">
+				<div class="form-group row">
+					<div class="col-xs-2">
+						<?php echo '<a href="principal.php?id='.$id.'" class="btn btn-warning" role="button">Cancelar</a>';?>
+					</div>
+					<div class="col-xs-2"></div>
+					<div class="col-xs-2"></div>
+					<div class="col-xs-2"></div>
+					<div class="col-xs-2"></div>
+					<div class="col-xs-2">
+						<button type="submit" id="enviar" class="btn btn-success" disabled="true" onclick="mio()" >Enviar Solicitud</button>
 					</div>
 				</div>
 			</div>
 		</form>
 	</div>
 
+	<script>
+		var state = false;
+		$("button[id=start]").click(function() {
+			state = true;
+			$("button[id=enviar]").attr("disabled", false);
+		});
 
-
-
+		function mio(){
+			var valores="";
+			var codigos="";
+			$("td").parents("tr").find("#name").each(function(){
+				valores+=$(this).html()+"_";
+			});
+			$("td").parents("tr").find("#code").each(function(){
+				codigos+=$(this).html()+"_";
+			});
+			asistentes.push(valores);
+			a_codigos.push(codigos);
+			$("#codigos").val(codigos);
+			$("#nombres").val(valores);
+		}
+		var asistentes = new Array();
+		var a_codigos = new Array();
+	</script>
 
 <!-- Modal -->
 	<div class="modal fade" id="myModal" role="dialog">
@@ -564,12 +512,7 @@ $("input[id=file-xml]").attr("required", true);
 				</div>
 				<div class="modal-body">
 					<div class="table-responsive" id="lista_autos">
-
-
 					</div>
-
-
-
 				</div>
 				<div class="modal-footer">
 					<button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
